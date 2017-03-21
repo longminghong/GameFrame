6cc0368cdae95bcf6d24d0d7e124af6061c86e5e
longminghong <longminghong@126.com>
2017-03-21 18:51:39 +0800

    initial project.
    create project.

---
+++ GameFrameCode/GameFrame/GameFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameFrame
+//
+//  Created by longminghong on 17/3/21.
+//  Copyright © 2017年 longminghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
