b2d8928af0ce0d5d46b732c4894717c1a0debf89
longminghong <longminghong@126.com>
2017-03-21 22:57:32 +0800

    添加文件，修改配置文件
    1. 添加component group
    2. 添加 component protocol 文件

---
+++ GameFrameCode/GameFrame/GameFrame/COMPONENTS/MHComponentsProtocol.h
@@ -0,0 +1,13 @@
+//
+//  MHComponentsProtocol.h
+//  GameFrame
+//
+//  Created by longminghong on 17/3/21.
+//  Copyright © 2017年 longminghong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MHComponentsProtocol <NSObject>
+
+@end
